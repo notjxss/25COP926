0d07ee07dbafadd76efbcac57e4ceb4fb01208e7
Jessica Cheung <jessicasmcheung@hotmail.com>
2026-05-20 09:55:25 +0100

    merge

--- src/components/factBox.jsx
+++ src/components/factBox.jsx
@@ -1,17 +1,11 @@
 import { useState, useEffect } from "react";
 
 export default function FactBox() {
-<<<<<<< HEAD
   // stores the random fact returned from backend
   const [fact, setFact] = useState("");
 
   useEffect(() => {
     // fetch a single random fact when component renders
-=======
-  const [fact, setFact] = useState("");
-
-  useEffect(() => {
->>>>>>> 47483494d92f92a77bb4ce98f599eff96fa131e5
     fetch("/backend/getRandomFact.php")
       .then(res => res.json())
       .then(data => setFact(data.fact));
